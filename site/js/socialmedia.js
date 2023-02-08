359a96969633798fd8e751d7217e36baf78f16ef
Migliorelli <114029690+miguel-mib@users.noreply.github.com>
2023-02-08 00:09:49 -0300

    preguiça

---
+++ site/js/socialmedia.js
@@ -0,0 +1,5 @@
+const instagramButton = document.querySelector(".instagram-button div")
+
+instagramButton.addEventListener("click", function () {
+    this.textContent = "Seguindo ✔"
+})
---
+++ site/js/voting.js
@@ -0,0 +1,51 @@
+// VOTAÇÃO DEFAULT
+function cloneParticipants() {
+    const names = [
+        "Miguel Migliorelli Bringhenti",
+        "Maysa Hellen Sacht Aragão",
+        "Miguel Força Mariani",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F"
+    ];
+    
+    for (var i = 0; i < 9; i++) {
+        let original = document.querySelector(".participant-card");
+        let clone = original.cloneNode(true);
+        let destino = document.querySelector(".voting-area");
+        
+        clone.style.display = "inline-block";
+        clone.querySelector("#vote-button").value = i;
+        clone.querySelector("#name").innerHTML = names[i];
+        destino.appendChild(clone);
+    }
+}
+cloneParticipants()
+
+const form_votacao = document.forms["voting-form"];
+const vote_spans   = document.querySelectorAll("#radio-label span");
+
+form_votacao.addEventListener("submit", function (event) {
+    let data = new FormData(form_votacao);
+    let output
+    for (const entry of data) {
+        output = `${entry[1]}\r`;
+    }
+    alert(output);
+    
+    event.preventDefault();
+});
+
+
+vote_spans.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        vote_spans.forEach(function (unslect) {
+            unslect.innerHTML = "SELECIONAR ";
+        });
+        
+        this.innerHTML = "SELECIONADO ✔";
+    });
+});
