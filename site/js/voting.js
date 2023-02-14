943df3988f5cce856905f3be9728b654e0be4842
Migliorelli <114029690+miguel-mib@users.noreply.github.com>
2023-02-13 21:20:57 -0300

    .

--- site/js/voting.js
+++ site/js/voting.js
@@ -1,51 +1,41 @@
 // VOTAÇÃO DEFAULT
 function cloneParticipants() {
-    const names = [
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I"
-    ];
-    
-    for (var i = 0; i < 9; i++) {
-        let original = document.querySelector(".participant-card");
-        let clone = original.cloneNode(true);
-        let destino = document.querySelector(".voting-area");
-        
-        clone.style.display = "inline-block";
-        clone.querySelector("#vote-button").value = i;
-        clone.querySelector("#name").innerHTML = names[i];
-        destino.appendChild(clone);
-    }
+  const names = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+  for (var i = 0; i < 8; i++) {
+    let original = document.querySelector(".participant-card");
+    let clone = original.cloneNode(true);
+    let destino = document.querySelector(".voting-area");
+
+    clone.style.display = "inline-block";
+    clone.querySelector("#vote-button").value = i;
+    clone.querySelector("#name").innerHTML = names[i];
+    destino.appendChild(clone);
+  }
 }
-cloneParticipants()
+cloneParticipants();
 
 const form_votacao = document.forms["voting-form"];
-const vote_spans   = document.querySelectorAll("#radio-label span");
+const vote_spans = document.querySelectorAll("#radio-label span");
 
 form_votacao.addEventListener("submit", function (event) {
-    let data = new FormData(form_votacao);
-    let output
-    for (const entry of data) {
-        output = `${entry[1]}\r`;
-    }
-    alert(output);
-    
-    event.preventDefault();
+  let data = new FormData(form_votacao);
+  let output;
+  for (const entry of data) {
+    output = `${entry[1]}\r`;
+  }
+  alert(output);
+
+  event.preventDefault();
 });
 
-
 vote_spans.forEach(function (item) {
-    item.addEventListener("click", function (e) {
-        vote_spans.forEach(function (unslect) {
-            unslect.innerHTML = "SELECIONAR ";
-        });
-        
-        this.innerHTML = "<span class=\"material-symbols-outlined\">check_circle</span>SELECIONADO";
+  item.addEventListener("click", function (e) {
+    vote_spans.forEach(function (unslect) {
+      unslect.innerHTML = "SELECIONAR ";
     });
+
+    this.innerHTML =
+      '<span class="material-symbols-outlined">check_circle</span>SELECIONADO';
+  });
 });
